b7b842657c8cddca41f3b219e1250c31b8f1779e
ivo <ivo.marti@edorex.ch>
2021-12-02 18:17:46 +0100

    d2

---
+++ 2021_02_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+var x = 0;
+var d = 0;
+
+foreach (var l in File.ReadAllLines("in.txt"))
+{
+  var prms = l.Split(' ');
+  var val = int.Parse(prms[1]);
+  switch (prms[0])
+  {
+    case "forward":
+      x += val;
+      break;
+    case "up":
+      d -= val;
+      break;
+    case "down":
+      d += val;
+      break;
+  }
+}
+
+Console.WriteLine("x={0},d={1},s={2}", x, d, x * d);
